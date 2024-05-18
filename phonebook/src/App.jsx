b6acb086ce8cde84024473f1186e022bb0a748a2
Pablo-fp <pablofp1988@hotmail.com>
2024-05-18 22:53:17 +0200

    The Phonebook Step 4

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'
-     }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
 
 
   const addPerson = (event) => {
@@ -26,6 +28,9 @@ const App = () => {
     }
   }
 
+  const personsToShow = persons.filter((person) => {
+    return person.name.toLowerCase().includes(searchName.toLowerCase());
+});
   
 
   const handleNameChange = (event) => {
@@ -38,50 +43,43 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setSearchName(event.target.value)
+  }
 
 
   return (
     <div>
       <h2>Phonebook</h2>
-        <div>filter shown with: 
+        <div>filter shown with: &nbsp;  
           <input
             type="text"
-            value={searchTerm}
+            value={searchName}
             onChange={handleSearchChange}
             placeholder="Search names"
-          />
-          <ul>
-            {filteredNames.map(person => (
-              <li key={person.id}>{person.name}</li>
-            ))}
-          </ul>  
+          />  
         </div>
       <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} required/>
+          name:&nbsp;&nbsp;&nbsp;<input value={newName} onChange={handleNameChange} required/>
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange} required/>
+          number:<input value={newNumber} onChange={handleNumberChange} required/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map((person, index) => {
-          return (
-            <div key={index}>
-              <p>{person.name} {person.number} </p>
-              
-            </div>
-        
-          )
-        })}
+      <ul>
+        {personsToShow.map((person, index) => (
+            <li key={index}>{person.name} {person.number}</li>
+          ))}
                
                     
-      </div>
+      </ul>
     </div>
   )
 }
